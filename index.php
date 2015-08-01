fd3832c8973a8452aac89029c0355a0436bae8e1
thiagoalvesmacedo <thiagoalvesmacedo@192.168.0.13>
2015-08-01 10:36:37 -0300

    primeiro commit

--- index.php
+++ index.php
@@ -1,36 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="description" content="Trezz Cosméticos">
-<meta name="author" content="tikovs">
-
-<title>Trezz Cosméticos</title>
-<link rel="icon" type="image/png" href="img/horta_icon.png" />
-
-
-<link href="http://fonts.googleapis.com/css?family=Montserrat:400,700"
-	rel="stylesheet" type="text/css">
-<link
-	href="http://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic"
-	rel="stylesheet" type="text/css">
-
-<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-<!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
+<head>  	
+	<meta http-equiv="content-type" content="text/html; charset=utf-8">  	
+	<title>Trezz Cosméticos</title>  	
+	<link rel="stylesheet" href="estilo.css" type="text/css" media="screen">  
 </head>
 
-<body id="page-top" class="index" >
-   <img src="IMG_1756.jpg" align="center">
-	<h1> Em Construção</h1>
+<body>
+<div id="img"><center>
+	<img src="IMG_1756.jpg" alt="Em construção" align="center">
+</div></center>
 </body>
-
 </html>
+
+
